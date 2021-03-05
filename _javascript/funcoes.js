4243cc9d87a7b8c4bc1b7c159ab9ce11afd1cd86
leonardohjx <leonardohjx@gmail.com>
2021-03-05 16:09:04 -0300

    atualizando nomes de pastas

--- _javascript/funcoes.js
+++
@@ -1,4 +0,0 @@
-//  Muda a foto do Menu superior da tela
-function mudaFoto (foto) {
-    document.getElementById('icone').src = foto;
-}
---
+++ javascript/funcoes.js
@@ -0,0 +1,4 @@
+//  Muda a foto do Menu superior da tela
+function mudaFoto (foto) {
+    document.getElementById('icone').src = foto;
+}
